3ebee3ad06f474fd68738327eda710824445b85e
tapadar13 <monsurismyname@gmail.com>
2024-09-10 12:22:19 +0530

    feat: add identification and verification fields in form
    - Integrated `FormFieldType.SELECT` to allow selection of identification type.
    - Added input for `identificationNumber` with custom placeholder.
    - Implemented `FileUploader` for uploading a scanned copy of identification document.
    - Updated `CustomFormField` to render file upload as a skeleton field.

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -13,9 +13,10 @@ import SubmitButton from "../SubmitButton";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Label } from "../ui/label";
-import { Doctors, GenderOptions } from "@/constants";
+import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
 import { SelectItem } from "@radix-ui/react-select";
 import Image from "next/image";
+import { FileUploader } from "../FileUploader";
 
 function RegisterForm({ user }: { user: User }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -254,6 +255,46 @@ function RegisterForm({ user }: { user: User }) {
               placeholder="Fractured arm in 2018, Had dengue fever in 2019"
             />
           </div>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Identification and Verfication</h2>
+          </div>
+
+          <CustomFormField
+            fieldType={FormFieldType.SELECT}
+            control={form.control}
+            name="identificationType"
+            label="Identification Type"
+            placeholder="Select identification type"
+          >
+            {IdentificationTypes.map((type, i) => (
+              <SelectItem key={type + i} value={type}>
+                {type}
+              </SelectItem>
+            ))}
+          </CustomFormField>
+
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="identificationNumber"
+            label="Identification Number"
+            placeholder="123456789"
+          />
+
+          <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="identificationDocument"
+            label="Scanned Copy of Identification Document"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <FileUploader files={field.value} onChange={field.onChange} />
+              </FormControl>
+            )}
+          />
         </section>
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
