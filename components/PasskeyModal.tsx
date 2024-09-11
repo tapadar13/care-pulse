34a03c78a4aef4d632a59dd3783a748f949625a3
tapadar13 <monsurismyname@gmail.com>
2024-09-11 12:11:24 +0530

    feat: implement automatic admin access verification with stored passkey
    - Added logic to `PasskeyModal` component to automatically verify stored admin passkey from localStorage.
    - Utilized `useEffect` to check the encrypted passkey upon component mount and decrypt it for validation against the environment variable `NEXT_PUBLIC_ADMIN_PASSKEY`.
    - If the decrypted passkey matches, the user is automatically redirected to the `/admin` route without needing to re-enter the passkey.
    - If the passkey is invalid or missing, the modal prompts for input again.
    - Integrated `useRouter` for navigation and `usePathname` to dynamically assess the current page path.

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import {
   AlertDialog,
@@ -18,13 +18,31 @@ import {
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
-import { encryptKey } from "@/lib/utils";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 export const PasskeyModal = () => {
   const router = useRouter();
+  const path = usePathname();
   const [open, setOpen] = useState(false);
   const [passkey, setPasskey] = useState("");
   const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessKey")
+      : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (path)
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY!.toString()) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setOpen(true);
+      }
+  }, [encryptedKey]);
 
   const closeModal = () => {
     setOpen(false);
